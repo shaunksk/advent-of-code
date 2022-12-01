1ea49fa2e81bfa9eb47b06de53cfcbb13ae5142f
shaunksk <51124570+shaunksk@users.noreply.github.com>
2022-12-01 22:22:21 +0000

    Update gpt3.py

--- gpt3.py
+++ gpt3.py
@@ -52,3 +52,59 @@ def main():
 
 if __name__ == '__main__':
     main()
+   
+##################################################
+# Second prompt
+
+"""
+Solve advent of code 2022 day 2
+"""
+
+import os
+from shared.readdayinput import readdayinput
+
+def first_half(dayinput):
+    """
+    Solve first half of day 2
+    """
+    valid_passwords = 0
+    for line in dayinput:
+        line = line.split()
+        min_max = line[0].split('-')
+        min_max = [int(x) for x in min_max]
+        letter = line[1][0]
+        password = line[2]
+        count = password.count(letter)
+        if count >= min_max[0] and count <= min_max[1]:
+            valid_passwords += 1
+    return valid_passwords
+
+def second_half(dayinput):
+    """
+    Solve second half of day 2
+    """
+    valid_passwords = 0
+    for line in dayinput:
+        line = line.split()
+        min_max = line[0].split('-')
+        min_max = [int(x) for x in min_max]
+        letter = line[1][0]
+        password = line[2]
+        if (password[min_max[0]-1] == letter) ^ (password[min_max[1]-1] == letter):
+            valid_passwords += 1
+    return valid_passwords
+
+def app():
+    """
+    runs day application
+    """
+    dayinput = readdayinput()
+    half_one = first_half(dayinput)
+    half_two = second_half(dayinput)
+    print(half_one, half_two)
+
+if __name__ == "__main__":
+    """
+    MAIN APP
+    """
+    app()
