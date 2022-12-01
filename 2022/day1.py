0a9202786f080acd22d4d228cd4939431450a053
Shaun <skoon@deloitte.co.uk>
2022-12-01 10:59:17 +0000

    day 1 refinement

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,22 +1,16 @@
 import os
-import sys
 
-if getattr(sys, 'frozen', False):
-    # If the application is run as a bundle, the PyInstaller bootloader
-    # extends the sys module by a flag frozen=True and sets the app 
-    # path into variable _MEIPASS'.
-    cwd = sys._MEIPASS
-else:
-    cwd = os.path.dirname(os.path.abspath(__file__))
-
-
+# Read in Data
+cwd = os.path.dirname(os.path.abspath(__file__))
 f = open(cwd+'/day1.txt', 'r')
 content = f.read()
 f.close()
 
+# Split out number groups by 2 line carriages - split each group into list and sum groups
 content = content.split('\n'+'\n')
 content = [x.split('\n') for x in content]
 content = [sum([int(x) for x in group]) for group in content]
 
+# Solution - max sum and sum of 3 highest sums
 print(max(content))
 print(sum(sorted(content)[-3:]))
