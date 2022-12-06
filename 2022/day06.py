466e7c1b52b53f5e3bf312c7c6b29f69b93e8102
Shaun <shaun.koon@hotmail.com>
2022-12-06 07:45:01 +0000

    day6 2022 complete

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,5 +1,20 @@
 import os
 f = open(os.path.dirname(os.path.abspath(__file__)) + '/day06.txt', 'r')
 content = f.read()
-print(content)
 f.close()
+
+# Example
+# content = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+
+def solve(len_marker):
+    for x in range(len(content)):
+        if len(content[x:x+len_marker]) == len(set(content[x:x+len_marker])):
+            print(x+len_marker)
+            print(content[x:x+len_marker])
+            break
+
+# Part 1
+solve(44)
+
+# Part 2
+solve(14)
